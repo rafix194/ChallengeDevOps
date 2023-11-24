9c09ca38cbc79457bf3c648c9b7ccd46281267fd
Rafael Cavalcanti <rafa.luiz.cavalcanti@hotmail.com>
2023-11-24 13:58:39 +1100

    Docker file host fix

--- application/config/database.php
+++ application/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3307'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'mysql.ckrolti37rrx.ap-southeast-2.rds.amazonaws.com',
+            'port' => '3306',
+            'database' => 'homestead',
+            'username' => 'homestead',
+            'password' => 'secret01',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
